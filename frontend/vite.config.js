a0ccf977bb88545a066302dbe0e0f9adb5b99a6c
Initial commit <lizhihong268810@gmail.com>
2025-05-11 21:24:45 +0800

    initial

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  base: '/test/',            // 和你 gh-pages 的子路径保持一致
+  base: '/test/',
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
