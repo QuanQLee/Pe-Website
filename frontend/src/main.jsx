e4b9e6ec61a5f0c225ba4a4a5af0643e7e16c7e9
Initial commit <lizhihong268810@gmail.com>
2025-05-11 18:25:38 +0800

    initial

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter as Router } from 'react-router-dom';
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';  // ★ 1. 用 HashRouter
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/test/">      {/* gh-pages 子路径 */}
+  <Router>       {/* 不需要 basename，# 就把路径隔开了 */}
     <App />
-  </BrowserRouter>
-)
+  </Router>
+);
